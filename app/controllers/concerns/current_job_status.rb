7740b4ad3959d5d2afd3eb53d476c72dda4b7f11
cohakim <cohakim@gmail.com>
2015-03-21 08:18:49 +0900

    Improve complexity method

--- app/controllers/concerns/current_job_status.rb
+++ app/controllers/concerns/current_job_status.rb
@@ -4,22 +4,22 @@ module CurrentJobStatus
   protected
 
   def current_status
-    {
+    Hashie::Mash.new.tap do |status|
       # server status
-      busyness: ServerStatus.busyness,
-      processing_job_count: ServerStatus.processing_job_count,
+      status.busyness             = ServerStatus.busyness
+      status.processing_job_count = ServerStatus.processing_job_count
       # job stats
-      job_state: current_user.active_job.progression.aasm.current_state,
-      statuses_count: 0, # TODO: 実装する
-      destroyed_count: 0,
-      failed_count: 0,
+      status.job_state            = current_user.active_job.progression.aasm.current_state
+      status.statuses_count       = 0 # TODO: 実装する
+      status.destroyed_count      = 0 # TODO: 実装する
+      status.failed_count         = 0 # TODO: 実装する
       # parameters
-      name: current_user.name,
-      posted_from: current_user.active_job.parameter.collect_from.try(:to_date),
-      posted_to: current_user.active_job.parameter.collect_to.try(:to_date),
-      protect_reply: current_user.active_job.parameter.protect_reply,
-      protect_favorite: current_user.active_job.parameter.protect_favorite,
-      created_at: current_user.active_job.created_at,
-    }
+      status.name             = current_user.name
+      status.posted_from      = current_user.active_job.parameter.collect_from.try(:to_date)
+      status.posted_to        = current_user.active_job.parameter.collect_to.try(:to_date)
+      status.protect_reply    = current_user.active_job.parameter.protect_reply?
+      status.protect_favorite = current_user.active_job.parameter.protect_favorite?
+      status.created_at       = current_user.active_job.created_at
+    end
   end
 end
