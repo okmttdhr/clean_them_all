df949db7e35adf9b458f0f4555525dfd1089b494
cohakim <cohakim@gmail.com>
2016-02-07 19:36:43 +0900

    デプロイ先の取得ロジックを変更

--- config/deploy/hotfix.rb
+++ config/deploy/hotfix.rb
@@ -1,6 +1,16 @@
-role :web,   'web01.kurorekishi.me'
-role :app,   'web01.kurorekishi.me'
-role :db,    'web01.kurorekishi.me', primary: true
+region       = ENV['AWS_REGION']
+filter_stage = "'Name=tag:Stage,Values=production'"
+filter_role  = "'Name=tag:Role,Values=*web*'"
+filter_state = "'Name=instance-state-name,Values=running'"
+filters      = [filter_stage, filter_role, filter_state].join(' ')
+query        = "'Reservations[].Instances[].[PrivateIpAddress]'"
+hosts        = %x( aws ec2 describe-instances --region=#{region} --filters #{filters} --query #{query} --output text ).split("\n")
+
+hosts.each do |host|
+  role :web, "manage.kurorekishi.me/#{host}"
+  role :app, "manage.kurorekishi.me/#{host}"
+end
+role :db,    "manage.kurorekishi.me/#{hosts.first}", primary: true
 
 set :deploy_env,  'production'
 set :unicorn_env, 'production'
@@ -8,9 +18,13 @@ set :rails_env,   'production'
 set :app_env,     'production'
 
 set :repository,  'https://github.com/cohakim/clean_them_all.git'
-set :deploy_via,  :remote_cache
 current_branch = `git branch`.match(/\* (\S+)\s/m)[1]
 set :branch, current_branch || 'master'
+set :deploy_via,  :remote_cache
+
+before 'deploy:update' do
+  run "rm -rf #{shared_path}/cached-copy"
+end
 
 require 'capistrano/deploy/tagger'
 set :latest_deploy_tag, 'inproduction'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,16 @@
-role :web,   'staging.kurorekishi.me'
-role :app,   'staging.kurorekishi.me'
-role :db,    'staging.kurorekishi.me', primary: true
+region       = ENV['AWS_REGION']
+filter_stage = "'Name=tag:Stage,Values=staging'"
+filter_role  = "'Name=tag:Role,Values=*web*'"
+filter_state = "'Name=instance-state-name,Values=running'"
+filters      = [filter_stage, filter_role, filter_state].join(' ')
+query        = "'Reservations[].Instances[].[PrivateIpAddress]'"
+hosts        = %x( aws ec2 describe-instances --region=#{region} --filters #{filters} --query #{query} --output text ).split("\n")
+
+hosts.each do |host|
+  role :web, "manage.kurorekishi.me/#{host}"
+  role :app, "manage.kurorekishi.me/#{host}"
+end
+role :db,    "manage.kurorekishi.me/#{hosts.first}", primary: true
 
 set :deploy_env,  'staging'
 set :unicorn_env, 'staging'
