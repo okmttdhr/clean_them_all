ad53adffcd9dc8c9a41714b8ec61c49254d4719d
cohakim <cohakim@gmail.com>
2017-06-26 12:34:51 +0900

    ValidなRSSを出力できていなかった問題を修正

--- engines/service/app/controllers/service/kpi_controller.rb
+++ engines/service/app/controllers/service/kpi_controller.rb
@@ -1,21 +1,27 @@
 module Service
   class KpiController < ApplicationController
+    def feed
+      @overview      = overview
+      @registrations = registrations
+      render :feed, content_type: 'application/rss'
+    end
+
+    private
+
     def overview
-      @item = {
+      {
         processing: Job.processing.count,
         collecting: Job.collecting.count,
         cleaning:   Job.cleaning.count,
         tweets:     TimelineFragment.count,
       }
-      render :overview, content_type: 'application/rss'
     end
 
     def registrations
-      @item = {
+      {
         users: User.count,
         jobs:  Job.count,
       }
-      render :registrations, content_type: 'application/rss'
     end
   end
 end
