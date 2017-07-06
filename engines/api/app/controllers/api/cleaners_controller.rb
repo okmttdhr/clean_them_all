321183217d70e2f2c562a88541207e9520183814
cohakim <cohakim@gmail.com>
2017-07-06 14:25:01 +0900

    セッションが切れている場合400を返すようにした

--- engines/api/app/controllers/api/cleaners_controller.rb
+++ engines/api/app/controllers/api/cleaners_controller.rb
@@ -1,8 +1,21 @@
 module Api
   class CleanersController < ApplicationController
+    before_action :user_should_be_authorized
+    before_action :user_should_have_active_job
+
     def show
       @job           = current_user.active_job
       @server_status = ServerStatus.current_server_status
+    end
+
+    private
+
+    def user_should_be_authorized
+      head :bad_request if current_user.blank?
+    end
+
+    def user_should_have_active_job
+      head :bad_request if current_user.active_job.blank?
     end
   end
 end
