a7b13a022745c6cc3605240084c785c7aa960e84
cohakim <cohakim@gmail.com>
2017-07-09 02:20:07 +0900

    Add asset_syc

---
+++ config/initializers/asset_sync.rb
@@ -0,0 +1,13 @@
+if defined?(AssetSync)
+  AssetSync.configure do |config|
+    config.fog_provider          = 'AWS'
+    Fog.credentials              = { path_style: true }
+    config.aws_access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    config.fog_directory         = configatron.aws.s3.bucket_name
+    config.fog_region            = ENV['AWS_REGION']
+    config.existing_remote_files = 'delete'
+    config.gzip_compression      = true
+    config.manifest              = true
+  end
+end
