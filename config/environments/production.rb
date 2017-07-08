140e01c592e7caad35770a54dfb9ca0ab49e699e
cohakim <cohakim@gmail.com>
2017-07-09 02:44:01 +0900

    [from now] 2017/07/09 02:44:01
    diff --git a/config/environments/production.rb b/config/environments/production.rb
    index 3f5b4a8..c468fd6 100644
    --- a/config/environments/production.rb
    +++ b/config/environments/production.rb
    @@ -14,8 +14,8 @@ Rails.application.configure do
       config.i18n.fallbacks                = true
       config.active_support.deprecation    = :notify
    
    -  config.action_controller.asset_host = "//s3.amazonaws.com/#{configatron.aws.s3.bucket_name}"
    -  config.assets.prefix = '/production/assets'
    +  # config.action_controller.asset_host = "//s3.amazonaws.com/#{configatron.aws.s3.bucket_name}"
    +  # config.assets.prefix = '/production/assets'
    
       config.log_formatter = ::Logger::Formatter.new
       if ENV["RAILS_LOG_TO_STDOUT"].present?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,17 +4,18 @@ Rails.application.configure do
   config.eager_load                        = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.public_file_server.enabled        = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.public_file_server.enabled    = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor          = :uglifier
   config.assets.compile                = false
+  config.assets.quiet                  = true
   config.log_level                     = :warn
   config.log_tags                      = [ :request_id ]
   config.action_mailer.perform_caching = false
   config.i18n.fallbacks                = true
   config.active_support.deprecation    = :notify
 
-  config.action_controller.asset_host = "//s3.amazonaws.com/#{configatron.aws.s3.bucket_name}"
+  config.action_controller.asset_host = "//s3.amazonaws.com/kurorekishi-me"
   config.assets.prefix = '/production/assets'
 
   config.log_formatter = ::Logger::Formatter.new
