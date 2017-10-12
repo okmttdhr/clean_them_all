d25213e03d5923487080c4323ac0dedb58596bc0
cohakim <cohakim@gmail.com>
2017-10-13 06:10:55 +0900

    ロックされる問題を修正

--- engines/backend/app/jobs/backend/sweeper_job.rb
+++ engines/backend/app/jobs/backend/sweeper_job.rb
@@ -1,11 +1,10 @@
 module Backend
   class SweeperJob < ApplicationJob
     def perform
-      TimelineFragment.where.not(job_id: Job.processing).limit(3200).pluck(:id).each do |id|
-        TimelineFragment.delete(id)
-      end
+      TimelineFragment.where.not(job_id: Job.processing).limit(3200).each(&:delete)
+
       # FIXME: 進捗状態が更新されない原因を突き止める
-      Job.cleaning.where(transition_state: [:confirming, :closing, :closed]).update_all(progression_state: :aborted)
+      Job.cleaning.where(transition_state: [:confirming, :closing, :closed]).each { |r| r.update(progression_state: :aborted) }
     end
   end
 end
